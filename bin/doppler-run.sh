d68694b2776ebe282e74364cdf0752d9cd5c1be1
Abhay Yadav <ayadav@c3controls.com>
2021-08-19 09:35:18 +0530

    First commit

---
+++ bin/doppler-run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# DOPPLER_TOKEN environment variable required for production and CI/CD environments
+doppler run -- node build/index.js
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const data = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+
+data.forEach(element => {
+  console.log(element);
+});
